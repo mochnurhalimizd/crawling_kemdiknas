8970228ab66f0f77ebc2410e9a129f47fe8ee874
irfanandriansyah1997 <irfanandriansyah10@gmail.com>
2018-02-02 08:47:21 +0700

    implement model for saving data

--- modules/provinsi/__init__.py
+++ modules/provinsi/__init__.py
@@ -3,9 +3,11 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
-from library.request.lib import RequestLib
 from library.log.lib import LoggerLib
 from library.config.lib import get_config
+from library.request.lib import RequestLib
+
+from model.provinsi import ProvinsiModel
 
 
 class ProvinsiModules:
@@ -13,6 +15,7 @@ class ProvinsiModules:
         self.logger = LoggerLib('provinsi_modules')
         self.request = RequestLib(self.logger)
         self.config = get_config('config.conf')
+        self.model = ProvinsiModel(self.get_parameter(self.config).get('database'), self.logger)
         self.data = self.get_parameter(self.config)
 
     def run(self):
@@ -23,7 +26,8 @@ class ProvinsiModules:
             return self.reprocess(data)
 
     def reprocess(self, data):
-        print(data)
+        for item in data:
+            self.model.search_data(item.get('kode_wilayah'), item)
 
     @staticmethod
     def get_parameter(config):
@@ -32,6 +36,11 @@ class ProvinsiModules:
             'parameter': {
                 'id_level_wilayah': config.get('level', 'provinsi'),
                 'semester_id': 20171
+            },
+            'database': {
+                'db': config.get('mongo', 'mongo_database_config'),
+                'host': config.get('mongo', 'mongo_url'),
+                'port': config.get('mongo', 'mongo_port')
             }
         }
 
